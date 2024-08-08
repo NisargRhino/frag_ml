4d62ce283eeee8b59d47f745d27f626370fb08c1
NisargRhino <141799170+NisargRhino@users.noreply.github.com>
2024-08-07 21:58:21 -0700

    Update train_updated_gpu.py

--- train_updated_gpu.py
+++ train_updated_gpu.py
@@ -16,7 +16,7 @@ print(f'device: {device}')
 
 # Load the dataset
 file_path = "./erk2.csv"
-data_org = pd.read_csv(file_path, nrows=100)
+data_org = pd.read_csv(file_path)
 
 # Custom dataset class
 class SMILESDataset(Dataset):
