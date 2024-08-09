e82ebc58a1a2ca0880b2a684faf956fb2c90740a
NisargRhino <141799170+NisargRhino@users.noreply.github.com>
2024-08-08 23:25:18 -0700

    Update train_updated2.py

--- train_updated2.py
+++ train_updated2.py
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f'device: {device}')
 
 # Load the dataset
-file_path = "/Users/nisargshah/Documents/cs/ml4/mTORcanonical.csv"
+file_path = "./mTORcanonical.csv"
 data_org = pd.read_csv(file_path, nrows=100)
 
 # Custom dataset class
